97eda24c13d02a50fc984393af23dcd892402784
cristinanagy <58230925+cristinanagy@users.noreply.github.com>
2019-12-03 08:20:11 +0200

    App.java @cristinanagy

---
+++ App.java
@@ -0,0 +1,12 @@
+package ca.demo.terminal;
+import org.joda.time.DateTime;
+
+public class App {
+    public static void main( String [] args){
+
+        // System.out.println("Hello!");
+        DateTime date = new DateTime();
+        System.out.println("Date:" + date);
+
+    }
+}
